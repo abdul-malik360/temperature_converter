7e097fa4b499d7293295ceae61d6bf5f6bc6251b
abdul-malik <abdulmalikmohamed360@gmail.com>
2021-05-07 16:49:52 +0200

    second commit

--- temp_converter-task.py
+++ temp_converter-task.py
@@ -15,10 +15,13 @@ celcius = Label(root, text="Fahrenheit To Celcius").place(x=420, y=100)
 entryF = Entry(root, textvariable=fahrenheit).place(x=120 , y=200)
 entryC = Entry(root, textvariable=celcius).place(x=420 , y=200)
 
+def ext():
+    return root.destroy()
+
 fbtn = Button(root, text="Activate - Fahrenheit", ).place(x=120, y=300)
 cbtn = Button(root, text="Activate - Celcius", ).place(x=420, y=300 )
 calconbtn = Button(root, text="Calculate Conversion", ).place(x=120, y=400 )
 clearbtn = Button(root, text="Clear", ).place(x=500, y=400 )
-extbtn = Button(root, text="Exit Program", ).place(x=450, y=500 )
+extbtn = Button(root, text="Exit Program",command=ext ).place(x=450, y=500 )
 
 root.mainloop()
